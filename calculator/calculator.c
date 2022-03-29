f9d0006d0cc01cd0735128ee6bea71ce47a1995e
Jayeen28 <jayeen6783@gmail.com>
2022-03-29 13:43:39 +0600

    calculator updated

---
+++ calculator/calculator.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	double num1;
+	double num2;
+	char op;
+	printf("Enter first number: ");
+	scanf("%lf",&num1);
+	printf("Enter operator: ");
+	scanf(" %c",&op);
+	printf("Enter second number: ");
+	scanf("%lf",&num2);
+	
+	if(op=='+'){
+		printf("%f",num1+num2);
+	}
+	else if(op=='-'){
+		printf("%f",num1-num2);
+	}
+	else if(op=='/'){
+		printf("%f",num1/num2);
+	}
+	else if(op=='*'){
+		printf("%f",num1*num2);
+	}
+	else{
+		printf("Invalid operator!");
+	}
+	return 0;
+}
